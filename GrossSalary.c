36634705c1392f899eb2764228a9535a685037e5
nirmalnishant645 <nirmalnishant645@gmail.com>
2019-08-28 18:42:19 +0530

    Program to calculate Gross Salary

---
+++ GrossSalary.c
@@ -0,0 +1,33 @@
+/*
+In a company an employee is paid as under:
+If his basic salary is less than Rs. 1500,
+then HRA = 10% of basic salary and DA = 90% of basic salary.
+If his basic salary is either equal to or above Rs. 1500,
+then HRA = Rs. 500 and DA = 98% of basic salary.
+If the employee's salary is input through the keyboard, WAP to find his gross salary.
+*/
+#include<stdio.h>
+int main()
+{
+    float bs, gs, da, hra;
+
+    printf("Enter the basic salary: ");
+    scanf("%f", &bs);
+
+    if(bs<1500)
+    {
+        hra=bs*10/100;
+        da=bs*90/100;
+    }
+    else
+    {
+        hra=500;
+        da=bs*98/100;
+    }
+
+    gs=bs+hra+da;
+    printf("Gross Salary: Rs. %f", gs);
+
+    return 0;
+    
+}
