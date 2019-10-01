bb701d9cc7ee3890235db8823fce7b5e74eef63a
Nishant Nirmal <nirmalnishant645@gmail.com>
2019-10-01 10:35:01 +0530

    FCFS Scheduling Algorithm

--- Operating-System/FCFS.c
+++ Operating-System/FCFS.c
@@ -1,120 +1,112 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct node
+// CPU Scheduling- FCFS
+#include<stdio.h>
+// Struct Storing all the Processes
+struct process
 {
-    int no;
-    float at, bt, pc, tat, wt, rt, rd;
-    struct node *next;
-} NODE; // Calling struct as NODE
+	int process;
+	int arrival;
+	int burst;
+	int wait;
+	int response;
+	int tat;
+}p[10];
 
-void create_insert(NODE **p, int no, float at, float bt, float *fr)
+// Main Function
+int main()
 {
-    // CREATE NODE
-    NODE *q, *r = *p;
-    q = (NODE *)malloc(sizeof(NODE));
-    q->no = no;
-    q->at = at;
-    q->bt = bt;
-    q->rt = *fr - at;
-    q->pc = *fr + bt;
-    q->tat = q->pc - at;
-    q->wt = q->tat - bt;
-    q->rd = q->tat / bt;
-    *fr = *fr + bt; // Update First Response for next NODE
+    int n,avwt=0,avtat=0,i,j,pos,max,temp,sum=0;
+    printf("\nEnter total number of processes:");
+    scanf("%d",&n);
 
-    // INSERT NODE AT END (Queue)
-    q->next = NULL;
-    if (*p == NULL)
-        *p = q;
-    else
+// Taking the Input from the User
+    for(i=0;i<n;i++)
     {
-        while (r->next != NULL)
-            r = r->next;
-        r->next = q;
+	p[i].process = i+1;
+    	printf("\n Process [%d] :\n",i+1);
+        printf("\n Arrival Time:");
+        scanf("%d",&p[i].arrival);
+        printf("\n Burst Time:");
+        scanf("%d",&p[i].burst);
     }
-}
+    
+// Sorting all the Processes according to their Arrival Time
+    
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(p[j].arrival<p[pos].arrival)
+            {
+                pos=j;
+            }
+        }
+ 
+        temp=p[i].arrival;
+        p[i].arrival=p[pos].arrival;
+        p[pos].arrival=temp;
 
-void gantt_chart(NODE *p, int process)
-{
-    int i;
-    NODE *r = p;
-    printf("\n\nGannt Chart:\n");
-    for (i = 1; i <= process; i++)
-        printf("----------------");
+        temp=p[i].burst;
+        p[i].burst=p[pos].burst;
+        p[pos].burst=temp;
+ 
+        temp=p[i].process;
+        p[i].process=p[pos].process;
+        p[pos].process=temp;
+    
+    }
+    p[0].wait = 0;
+// Calculating Waiting and Response Time
+    for(i=1;i<n;i++)
+    {
+	sum = sum + p[i-1].burst;
+        p[i].wait = sum - p[i].arrival ; 
+	if(p[i].wait <0)
+		p[i].wait= 0;
+        p[i].response = p[i].wait;
+    }
+ 
+    printf("\nProcess\t\tArrival Time\tBurst Time\tResponse Time\tWaiting Time\tTurnaround Time");
+ 
+// Calculating Turn Around Time and Printing the Table
+    for(i=0;i<n;i++)
+    {
+        p[i].tat=p[i].burst+p[i].wait;
+        avwt+=p[i].wait;
+        avtat+=p[i].tat;
+        printf("\nP[%d]\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d",p[i].process,p[i].arrival,p[i].burst,p[i].response,p[i].wait,p[i].tat);
+    }
+ 
+    avwt/=i;
+    avtat/=i;
+    printf("\n\nAverage Waiting Time:%d",avwt);
+    printf("\nAverage Turnaround Time:%d\n\n",avtat);
+
+    printf("\n\n");
+
+    printf("Gantt Chart:\n-");
+
+    for(i=0;i<n;i++) {
+
+	printf("----------------");
+
+    }
+ 
     printf("\n");
 
-    for (i = 1; i <= process; i++)
-    {
-        printf("|\tP%d\t", p->no);
-        p = p->next;
-    }
-    printf("|\n");
+    for(i=0;i<n;i++) {
 
-    for (i = 1; i <= process; i++)
-        printf("----------------");
+	printf("\tP%d\t|",p[i].process);
 
-    printf("\n");
-    printf("%.1f      \t", r->at);
-    for (i = 1; i <= process; i++)
-    {
-        printf("%.1f      \t", r->pc);
-        r = r->next;
-    }
-}
-
-void display(NODE *p, int process)
-{
-    float ttat, twt, trd, trt, tbt;
-    ttat = twt = trd = trt = tbt = 0;
-    printf("\n\n\nProcess Details:");
-    while (p != NULL)
-    {
-        printf("\n Process %d: \n", p->no);
-        printf("\tArrival Time: %.2f\n", p->at);
-        printf("\tBurst Time: %.2f\n", p->bt);
-        printf("\tTurn Around Time: %.2f\n", p->tat);
-        printf("\tWaiting Time: %.2f\n", p->wt);
-        printf("\tRelative Delay: %.2f\n", p->rd);
-        printf("\tResponse Time: %.2f\n", p->rt);
-        // Storing the Total value for average
-        ttat += p->tat; // ttat = ttat + p->tat
-        twt += p->wt;
-        trd += p->rd;
-        trt += p->rt;
-        tbt += p->bt;
-
-        p = p->next;
-    }
-    printf("\n\n\nOverall Details:\n");
-    printf(" Throughput: %.2f\n", process / tbt);
-    printf(" Average Turn Around Time: %.2f\n", ttat / process);
-    printf(" Average Waiting Time: %.2f\n", twt / process);
-    printf(" Average Relative Delay: %.2f\n", trd / process);
-    printf(" Average Response Time: %.2f\n", trt / process);
-}
-
-void main()
-{
-    NODE *head = NULL;
-    int process, i;
-    float arrival_time, burst_time, first_response;
-    printf("Enter the no. of Process: ");
-    scanf("%d", &process);
-    for (i = 1; i <= process; i++)
-    {
-        printf("\nEnter the Details for Process %d: \n", i);
-        printf("\tArrival Time: ");
-        scanf("%f", &arrival_time);
-        printf("\tBurst Time: ");
-        scanf("%f", &burst_time);
-        if (i == 1 && arrival_time == 0)
-            first_response = arrival_time;
-        create_insert(&head, i, arrival_time, burst_time, &first_response);
     }
 
-    printf("\n<-------------------------------------------------  START ------------------------------------->\n");
-    gantt_chart(head, process);
-    display(head, process);
-    printf("\n<--------------------------------------------------  END -------------------------------------->\n");
+    printf("\n0");
+
+    for(i=0;i<n;i++) {
+	
+	printf("---------------%d",p[i].tat+p[i].arrival);
+
+    }
+ 
+    return 0;
 }
